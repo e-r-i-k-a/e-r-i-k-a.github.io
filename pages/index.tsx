60820edcb825066eeb5073d24a0aa91b40240025
e-r-i-k-a <samuels.erika@yahoo.com>
2022-06-19 10:10:18 -0400

    add day

--- pages/index.tsx
+++ pages/index.tsx
@@ -3,11 +3,12 @@ import Image from 'next/image';
 import styles from '../styles/home.module.css';
 
 const Home: NextPage = () => {
+  const day:string = (new Date().toLocaleDateString('en-US', { weekday: 'long' }).toLowerCase());
+
   return (
     <div className={styles.container}>
       <main className={styles.main}>
-        <p className={styles.title}>i love you</p>
-
+        <p className={styles.title}>happy {day}</p>
         <Image
           src='/images/bc-headpat.gif'
           alt='headpat'
