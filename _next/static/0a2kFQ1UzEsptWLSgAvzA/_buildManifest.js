3e63816724d5c3578a27168acafc51a581bfb5be
e-r-i-k-a <31043344+e-r-i-k-a@users.noreply.github.com>
2022-06-19 14:11:08 +0000

    Deploying to gh-pages from @ e-r-i-k-a/e-r-i-k-a.github.io@60820edcb825066eeb5073d24a0aa91b40240025 🚀

---
+++ _next/static/0a2kFQ1UzEsptWLSgAvzA/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/css/973383393ff569a1.css","static/chunks/pages/index-dc80c4a2c00d1a5d.js"],"/_error":["static/chunks/pages/_error-0a004b8b8498208d.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
