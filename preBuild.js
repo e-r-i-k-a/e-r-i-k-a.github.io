440b96c5705882db1010c4b0f23c0254d3cd5d48
e-r-i-k-a <31043344+e-r-i-k-a@users.noreply.github.com>
2022-06-20 22:13:20 +0000

    Deploying to gh-pages from @ e-r-i-k-a/e-r-i-k-a.github.io@b83e2660c1e27fc2c4ab663298dc084ce641017f 🚀

---
+++ preBuild.js
@@ -0,0 +1,27 @@
+const fs = require('fs');
+const path = require('path');
+var sizeOf = require('image-size');
+const dirPath = path.join(
+  __dirname,
+  'images'
+);
+
+async function getImages() {
+  fs.unlink(path.join(dirPath, 'data.json'), function cb(err, __) {
+    if (err) { } // do nothing
+  });
+  const images = [];
+  fs.readdirSync(dirPath).forEach(file => {
+    const { height, width } = sizeOf(path.join(dirPath, file));
+    images.push({
+      src: `/images/${file}`,
+      width: Number(width),
+      height: Number(height),
+    });
+  });
+  fs.writeFileSync(path.join(dirPath, 'data.json'), JSON.stringify(images));
+}
+
+(async () => {
+  const data = await getImages();
+})();
