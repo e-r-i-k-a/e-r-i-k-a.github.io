74e886c590d7ac20388cf4ad096bd63febeb5709
e-r-i-k-a <samuels.erika@yahoo.com>
2022-06-20 19:02:46 -0400

    randomize image based on date

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,23 +1,26 @@
 import type { NextPage } from 'next';
 import Image from 'next/image';
 import styles from '../styles/home.module.css';
-import data from '../public/images/data.json';
+import { Images } from '../types';
+import { getImage } from '../util/helpers';
+const images: Images = require('../public/images/data.json');
 
 const Home: NextPage = () => {
   const day: string = new Date()
     .toLocaleDateString('en-US', { weekday: 'long' })
     .toLowerCase();
+  const { src, width, height } = getImage(images);
 
   return (
     <div className={styles.container}>
       <main className={styles.main}>
         <p className={styles.title}>happy {day}</p>
         {
-          data.length && <Image
-          src={data[0].src}
+          src && <Image
+          src={src}
           alt='headpat'
-          width={data[0].width}
-          height={data[0].height}
+          width={width}
+          height={height}
           />
         }
       </main>
