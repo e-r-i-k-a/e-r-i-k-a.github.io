43d7a0f590b0b8cbf951b309ad7f1bc7a706913a
e-r-i-k-a <31043344+e-r-i-k-a@users.noreply.github.com>
2022-06-21 00:28:03 +0000

    Deploying to gh-pages from @ e-r-i-k-a/e-r-i-k-a.github.io@ee61cd36a630b312f8747343009e01351190f651 🚀

---
+++ _next/static/A69zoaA0850QFrNvuJc6z/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/css/459eb21ba81726ab.css","static/chunks/pages/index-88dbd8d45116b06e.js"],"/_error":["static/chunks/pages/_error-0a004b8b8498208d.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
--- _next/static/Hs8a8F_-qjMaXQcpURzRw/_buildManifest.js
+++
@@ -1,1 +0,0 @@
-self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/chunks/282-0acb5f523635f730.js","static/css/973383393ff569a1.css","static/chunks/pages/index-ef76456b6fd3cd83.js"],"/_error":["static/chunks/pages/_error-0a004b8b8498208d.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
